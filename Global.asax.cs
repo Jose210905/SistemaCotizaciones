dd54e4de0c8926c193709bc4896c652f237fa4bf
Jose210905 <164114744+Jose210905@users.noreply.github.com>
2025-08-15 11:06:30 -0600

    Add files via upload

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.Optimization;
-using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace SistemaCotizaciones
 {
@@ -14,8 +8,27 @@ namespace SistemaCotizaciones
         void Application_Start(object sender, EventArgs e)
         {
             // Código que se ejecuta al iniciar la aplicación
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            // Sistema de Cotizaciones iniciado correctamente
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            // Código que se ejecuta al finalizar la aplicación
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Código que se ejecuta cuando se produce un error no controlado
+        }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            // Código que se ejecuta cuando se inicia una nueva sesión
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+            // Código que se ejecuta cuando finaliza una sesión
         }
     }
 }
