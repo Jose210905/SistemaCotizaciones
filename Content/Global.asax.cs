40092584dd01ec8b2c2ca4400c316d63b9f3e526
Jose210905 <164114744+Jose210905@users.noreply.github.com>
2025-08-15 17:24:33 -0600

    Add files via upload

---
+++ Content/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Web;
+
+namespace SistemaCotizaciones
+{
+    public class Global : HttpApplication  
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            
+        }
+
+        void Session_Start(object sender, EventArgs e)
+        {
+           
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+            
+        }
+     }
